64bfc14ba93c5482f497753c378c789427f3d06d
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2023-03-19 23:25:47 +0500

    added a patchpackage for react-native-pixel-color and build successful on ios

--- src/screens/skinDetector.js
+++ src/screens/skinDetector.js
@@ -1,30 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Button, Image} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
 import {getPixelRGBA} from 'react-native-pixel-color';
 
 const SkinDetector = () => {
   const [image, setImage] = useState(null);
   const [isSkinDetected, setIsSkinDetected] = useState(false);
   useEffect(() => {
-    (async () => {
-      const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    })();
+    // (async () => {
+    //   const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    //   if (status !== 'granted') {
+    //     alert('Sorry, we need camera roll permissions to make this work!');
+    //   }
+    // })();
   }, []);
   const pickImage = async () => {
-    let result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-    if (!result.cancelled) {
-      setImage(result.uri);
-      detectSkin(result.uri);
-    }
+    // let result = await ImagePicker.launchCameraAsync({
+    //   mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    //   allowsEditing: true,
+    //   aspect: [4, 3],
+    //   quality: 1,
+    // });
+    // if (!result.cancelled) {
+    //   setImage(result.uri);
+    //   detectSkin(result.uri);
+    // }
   };
   const detectSkin = async uri => {
     const pixelColor = await getPixelRGBA(uri, 0, 0);
